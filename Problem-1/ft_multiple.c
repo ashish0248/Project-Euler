dd528f95541a504a05768428e8310e78975bc098
Your Name <hajime@hajimenoMacBook-Pro.local>
2021-03-08 09:29:16 +0900

    problem-1

---
+++ Problem-1/ft_multiple.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_multiple(void)
+{
+	int sum;
+	int num;
+
+	sum = 0;
+	num = 1;
+
+	while(num < 1000)
+	{
+		if(num % 3 == 0)
+		{
+			sum += num;
+		}
+
+		if(num % 5 == 0)
+		{
+			sum += num;
+		}
+
+		if(num % 15 == 0)
+		{
+			sum = sum - num;
+		}
+		num++;
+	}
+
+	printf("%d", sum);
+
+}
---
+++ Problem-1/main.c
@@ -0,0 +1,15 @@
+// https://odz.sakura.ne.jp/projecteuler/?Problem+1
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_multiple(void);
+
+int main(void)
+{
+
+	ft_multiple();
+
+	return (0);
+}
