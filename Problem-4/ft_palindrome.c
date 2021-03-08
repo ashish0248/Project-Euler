0cd67904a402bae54401710fc0029f8b8b14c255
Ashish <248ashish@gmail.com>
2021-03-09 01:22:21 +0900

    adjust problem-4

---
+++ Problem-4/ft_palindrome.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+// 配列を使ってtimesの桁を逆に並び替える関数
+int	ft_rev_times(int times)
+{
+	int array[6];
+	int rev_times;
+	int digit;
+	int count;
+
+	count = 0;
+	rev_times = 0;
+	digit = 100000;
+
+
+	while(count < 6)
+	{
+		// 一桁ずつ配列に入れる
+		array[count] = times / digit;
+		times = times % digit;
+		digit = digit / 10;
+		count++;
+	}
+
+	// 配列を逆から取り出す
+	digit = 100000;
+	while(count > 0)
+	{
+		rev_times =  rev_times + array[count - 1] * digit;
+		digit = digit / 10;
+		count--;
+	}
+
+	// 五桁の時先頭が0となってしまうので戻す
+	if(array[0] == 0)
+	{
+		rev_times = rev_times / 10;
+	}
+
+	return rev_times;
+}
+
+
+
+void	ft_palindrome(void)
+{
+	int num1;
+	int num2;
+	int times;
+	int rev_times;
+
+	num1 = 100;
+	num2 = 100;
+
+	while(num1 < 1000)
+	{
+		while(num2 < 1000)
+		{
+			times = num1 * num2;
+
+			rev_times = ft_rev_times(times);
+
+			if(times - rev_times == 0)
+			{
+				printf("%dと%dの積は%dで回文数です\n", num1, num2, rev_times);
+			}
+			num2++;
+
+		}
+		num2 = 100;
+		num1++;
+	}
+}
+
---
+++ Problem-4/main.c
@@ -0,0 +1,14 @@
+// https://odz.sakura.ne.jp/projecteuler/?Problem+4
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_palindrome(void);
+
+int main(void)
+{
+
+	ft_palindrome();
+
+	return (0);
+}
