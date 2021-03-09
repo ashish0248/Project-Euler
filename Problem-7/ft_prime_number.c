a12196ee0ec8f436ad08a21636b7c90d36c13a2f
Ashish <248ashish@gmail.com>
2021-03-09 09:14:23 +0900

    problem-7

---
+++ Problem-7/ft_prime_number.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+void ft_prime_number(int count)
+{
+	long prime_num;
+	long num;
+	int counted;
+
+	prime_num = 3;
+	num = 2;
+	counted = 1;
+
+	while (counted < count )
+	{
+		while( num < prime_num)
+		{
+			if(prime_num % num == 0)
+			{
+				prime_num += 2;
+				num = 2;
+			}else{
+				num++;
+			}
+		}
+		prime_num += 2;
+		num = 2;
+		counted++;
+	}
+
+	if(count ==1)
+	{
+		printf("%d番目の素数は2です\n", count);
+	}else{
+		printf("%d番目の素数は%ldです\n", count, prime_num-2);
+	}
+
+}
---
+++ Problem-7/main.c
@@ -0,0 +1,25 @@
+// https://odz.sakura.ne.jp/projecteuler/?Problem+7
+
+#include <stdio.h>
+#include <time.h>
+
+void ft_prime_number(int count);
+
+int main(void)
+{
+	int count;
+
+	printf("何番目の素数を調べますか？\n");
+	scanf("%d", &count);
+
+	clock_t start,end;
+	start = clock();
+
+
+		ft_prime_number(count);
+
+	end = clock();
+	printf("%.2f秒かかりました\n", (double)(end-start)/CLOCKS_PER_SEC);
+
+	return (0);
+}
